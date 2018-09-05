edf6365888d97a7a025a8acaf68870976bd994a4
RosaRend <ice2meetu@outlook.com>
2018-09-04 23:49:04 -0400

    Small changes

--- script/Canvas.js
+++ script/Canvas.js
@@ -4,36 +4,36 @@
   var musicNoteImages = ['./images/purple_note.png', './images/green_note.png', './images/purple_note2.png', './images/green_note2.png'];
   var img = new Image;
 
-/////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
 
-var MusicNote = function() {
-  this.startingX = startingPosition[Math.floor(Math.random() *4)];
-  this.startingY = 530;
-  this.startingH = 90;
-  this.startingW = 100;
-  this.src = musicNoteImages[[Math.floor(Math.random() *4)]];
-}
+  var MusicNote = function () {
+    this.startingX = startingPosition[Math.floor(Math.random() * 4)];
+    this.startingY = 530;
+    this.startingH = 90;
+    this.startingW = 100;
+    this.src = musicNoteImages[[Math.floor(Math.random() * 4)]];
+  }
 
-/////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
 
-MusicNote.prototype.moveNote = function(){
+  MusicNote.prototype.moveNote = function () {
 
-  setInterval(()=>{
+    setInterval(() => {
 
-    // for(var i=0; i < musicNotes.length; i++){
+      // for(var i=0; i < musicNotes.length; i++){
       // console.log("musicNotes[i]============== ", this.startingY)
       this.startingY -= 20;
       //  }
-  }, 200)
+    }, 200)
   };
 
 
-  function drawNotes(){
-    musicNotes.forEach(function(k){
+  function drawNotes() {
+    musicNotes.forEach(function (k) {
       var img = new Image();
       img.src = k.src;
 
-      img.onload = function(){
+      img.onload = function () {
         ctx.drawImage(img, k.startingX, k.startingY, k.startingW, k.startingH);
       }
 
@@ -41,130 +41,130 @@ MusicNote.prototype.moveNote = function(){
   }
   /////////////////////////////////////////////////////////////////////
 
- function newNote() {
+  function newNote() {
     that = this;
-    var random = Math.floor(1+ Math.random()*2); 
-    for(var i = 0; i < random; i++){  
+    var random = Math.floor(1 + Math.random() * 2);
+    for (var i = 0; i < random; i++) {
       var theNote = new MusicNote();
       musicNotes.push(theNote);
-      
+
       theImage = new Image();
-      theImage.src = theNote.src; 
-        
-          ctx.drawImage(theImage, theNote.startingX, theNote.startingY, theNote.startingW, theNote.startingH);
-          theNote.moveNote();
-                }
-    
- }
+      theImage.src = theNote.src;
 
-///////////////////////////////////////////////////////////////////////
+      ctx.drawImage(theImage, theNote.startingX, theNote.startingY, theNote.startingW, theNote.startingH);
+      theNote.moveNote();
+    }
 
-function dividers(){
-  var ctx = document.getElementById('IronGame').getContext('2d');
-
-  ctx.beginPath();
-  ctx.moveTo(180, 0);
-  ctx.lineTo(180, 720);
-  ctx.lineWidth = 2;
-  ctx.stroke();
-  
-  ctx.beginPath();
-  ctx.moveTo(360, 0);
-  ctx.lineTo(360, 720);
-  ctx.lineWidth = 2;
-  ctx.stroke();
-  
-  ctx.beginPath();
-  ctx.moveTo(540, 0);
-  ctx.lineTo(540, 720);
-  ctx.lineWidth = 2;
-  ctx.stroke();      
-  
-  ctx.beginPath();
-  ctx.moveTo(720, 0);
-  ctx.lineTo(720, 720);
-  ctx.lineWidth = 2;
-  ctx.stroke();
-}
-////////////////////////////////////////////////////////
-function newImg1 () {
-
-  var ctx = document.getElementById('IronGame').getContext('2d');
-
-  var img = new Image();
-  var startingX = 0;
-  var startingY = 0;
-  var startingW = 180;
-  var startingH = 100;
-  
-  img.onload = function(){
-  ctx.drawImage(img, startingX, startingY, startingW, startingH); 
-};
-
-img.src = './images/waves.png';
-};
- 
-function newImg2 () {
-  
-  var ctx = document.getElementById('IronGame').getContext('2d');
-  
-  var img = new Image();
-  var startingX = 0;
-  var startingY = 0;
-  var startingW = 180;
-  var startingH = 100;
-  
-img.onload = function(){
-  
-  ctx.drawImage(img, startingX + 180, startingY, startingW, startingH);
-
-};
-img.src = './images/waves.png';
-};
-
-function newImg3 () {
-  
-  var ctx = document.getElementById('IronGame').getContext('2d');
-  
-  var img = new Image();
-  var startingX = 0;
-  var startingY = 0;
-  var startingW = 180;
-  var startingH = 100;
-  
-  img.onload = function(){
-    
-    ctx.drawImage(img, startingX + 360, startingY, startingW, startingH);
-    
   }
 
-img.src = './images/waves.png';
-};
+  ///////////////////////////////////////////////////////////////////////
 
-function newImg4 () {
-  
-  var ctx = document.getElementById('IronGame').getContext('2d');
-  
-  var img = new Image();
-  var startingX = 0;
-  var startingY = 0;
-  var startingW = 180;
-  var startingH = 100;
-  
-  img.onload = function(){
-    
-  ctx.drawImage(img, startingX + 540, startingY, startingW, startingH);
-  
-};
-img.src = './images/waves.png';
-};
-//////////////////////////////////////////////////////////////////////
-function pointZone () {
-  var ctx = document.getElementById('IronGame').getContext('2d');
-  
-  ctx.beginPath();
-  ctx.moveTo(0, 100);
-  ctx.lineTo(720, 100);
-  ctx.stroke();
-}
+  function dividers() {
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(180, 0);
+    ctx.lineTo(180, 720);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(360, 0);
+    ctx.lineTo(360, 720);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(540, 0);
+    ctx.lineTo(540, 720);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(720, 0);
+    ctx.lineTo(720, 720);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+  ////////////////////////////////////////////////////////
+  function newImg1() {
+
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    var img = new Image();
+    var startingX = 0;
+    var startingY = 0;
+    var startingW = 180;
+    var startingH = 100;
+
+    img.onload = function () {
+      ctx.drawImage(img, startingX, startingY, startingW, startingH);
+    };
+
+    img.src = './images/waves.png';
+  };
+
+  function newImg2() {
+
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    var img = new Image();
+    var startingX = 0;
+    var startingY = 0;
+    var startingW = 180;
+    var startingH = 100;
+
+    img.onload = function () {
+
+      ctx.drawImage(img, startingX + 180, startingY, startingW, startingH);
+
+    };
+    img.src = './images/waves.png';
+  };
+
+  function newImg3() {
+
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    var img = new Image();
+    var startingX = 0;
+    var startingY = 0;
+    var startingW = 180;
+    var startingH = 100;
+
+    img.onload = function () {
+
+      ctx.drawImage(img, startingX + 360, startingY, startingW, startingH);
+
+    }
+
+    img.src = './images/waves.png';
+  };
+
+  function newImg4() {
+
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    var img = new Image();
+    var startingX = 0;
+    var startingY = 0;
+    var startingW = 180;
+    var startingH = 100;
+
+    img.onload = function () {
+
+      ctx.drawImage(img, startingX + 540, startingY, startingW, startingH);
+
+    };
+    img.src = './images/waves.png';
+  };
+  //////////////////////////////////////////////////////////////////////
+  function pointZone() {
+    var ctx = document.getElementById('IronGame').getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(0, 100);
+    ctx.lineTo(720, 100);
+    ctx.stroke();
+  }
   //////////////////////////////////////////////////////////////////////////////
--- script/Game.js
+++ script/Game.js
@@ -1,38 +1,38 @@
 // console.log(musicNotes)
 
 var newGame = new IronGame();
-  
-function IronGame(){
+
+function IronGame() {
   // this.keysClicked = [];
   //          
-  this.score = 0; 
+  this.score = 0;
 };
 
-IronGame.prototype.gameLost = function() {
-  if(newGame.score == -500){
-    return alert('I\'m gonna stop you right there. Maybe this game isn\'t right for you.' );
+IronGame.prototype.gameLost = function () {
+  if (newGame.score == -300) {
+    return alert('I\'m gonna stop you right there. Maybe this game isn\'t right for you.');
   }
 };
- var myMusic;
+var myMusic;
 
- function sound(src){
-   this.sound = document.createElement("audio");
-   this.sound.src = src;
-   this.sound.setAttribute("preload", "auto");
-   this.sound.setAttribute("controls", "none");
-   this.sound.style.display = "none";
-   document.body.appendChild(this.sound);
-   this.play = function(){
-     this.sound.play();
-   }
-   this.stop = function(){
-     this.sound.pause();
-   }
- }
- 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  }
+  this.stop = function () {
+    this.sound.pause();
+  }
+}
+
 //setInterval = 325000 = 3.25 song duration
 
- function startGame(){
+function startGame() {
   newGame = new IronGame();
 
   newGame.gameLost();
@@ -40,26 +40,26 @@ IronGame.prototype.gameLost = function() {
   myMusic.play();
   update();
 };
-var timeLeft = 4900;
+var timeLeft = 4500;
 
 var frames = 0;
-function update(){
-  setInterval(function(){
-    if(timeLeft >= 0){
-      timeLeft --;
-      ctx.clearRect(0,0,720,645)
+
+function update() {
+  setInterval(function () {
+    if (timeLeft >= 0) {
+      timeLeft--;
+      ctx.clearRect(0, 0, 720, 645)
       // theMusicNotes = new MusicNote();
       newImg1();
       newImg2();
       newImg3();
       newImg4();
-      
+
       dividers();
-      
+
       pointZone();
-      
-      
-      if (frames % 40 === 0){
+
+      if (frames % 40 === 0) {
         newNote();
       }
       drawNotes();
@@ -70,76 +70,70 @@ function update(){
   }, 50)
 }
 
-function updateScore(){
+function updateScore() {
   console.log(newGame.score)
   document.getElementById("score-board").innerHTML = newGame.score;
-// }
-// function update score{
-//   span => newGame.score
+  // }
+  // function update score{
+  //   span => newGame.score
 }
 
 
 
-document.onkeydown = function(e){
+document.onkeydown = function (e) {
   var p = false;
   var b = false;
 
-  switch(e.keyCode){
+  switch (e.keyCode) {
     case 65:
-    musicNotes.forEach(function(oneNote){
-      if(0 < oneNote.startingY && oneNote.startingY < 24 && 0 < oneNote.startingX && oneNote.startingX < 180){
-        p = true;
-      }
-      else if(25 < oneNote.startingY && oneNote.startingY < 50 && 0 < oneNote.startingX && oneNote.startingX< 180){
-        b = true;
-      }
-    })
-    break;
+      musicNotes.forEach(function (oneNote) {
+        if (0 < oneNote.startingY && oneNote.startingY < 24 && 0 < oneNote.startingX && oneNote.startingX < 180) {
+          p = true;
+        } else if (25 < oneNote.startingY && oneNote.startingY < 50 && 0 < oneNote.startingX && oneNote.startingX < 180) {
+          b = true;
+        }
+      })
+      break;
     case 83:
-    musicNotes.forEach(function(oneNote){
-      if(0 < oneNote.startingY && oneNote.startingY < 24 && 181 < oneNote.startingX && oneNote.startingX < 360){
-        p = true;
-      }
-      else if(25 < oneNote.startingY && oneNote.startingY < 50 && 181 < oneNote.startingX && oneNote.startingX < 360){
-        b = true;
-      }
-   
-    })
-    break;
-    case 68:
-    musicNotes.forEach(function(oneNote){
-    if(0 < oneNote.startingY && oneNote.startingY < 24 && 361 < oneNote.startingX && oneNote.startingX < 540){
-      p = true;
-    }
-    else if(25 < oneNote.startingY && oneNote.startingY < 50 && 361 < oneNote.startingX && oneNote.startingX < 540){
-      b = true;
-    }
-  })
-    break;
-    case 70:
-    musicNotes.forEach(function(oneNote){
-      if(0 < oneNote.startingY && oneNote.startingY < 100 && 541 < oneNote.startingX && oneNote.startingX < 720){
-        p = true;
-      // IronGame.ctx.clearRect(this.x, this.y, this.width, this.height);
-    }
-    else if(25 < oneNote.startingY && oneNote.startingY < 50 && 541 < oneNote.startingX && oneNote.startingX < 720){
-      b = true;
-    }
-  })
-  break;
-}
+      musicNotes.forEach(function (oneNote) {
+        if (0 < oneNote.startingY && oneNote.startingY < 24 && 181 < oneNote.startingX && oneNote.startingX < 360) {
+          p = true;
+        } else if (25 < oneNote.startingY && oneNote.startingY < 50 && 181 < oneNote.startingX && oneNote.startingX < 360) {
+          b = true;
+        }
 
-if(p){
-  newGame.score += 100;
-}
-else if(b){
-  newGame.score += 40;
-}
-else{
-  newGame.score -= 25;
-}
-updateScore();
-console.log( 'your score: '+ newGame.score);
+      })
+      break;
+    case 68:
+      musicNotes.forEach(function (oneNote) {
+        if (0 < oneNote.startingY && oneNote.startingY < 24 && 361 < oneNote.startingX && oneNote.startingX < 540) {
+          p = true;
+        } else if (25 < oneNote.startingY && oneNote.startingY < 50 && 361 < oneNote.startingX && oneNote.startingX < 540) {
+          b = true;
+        }
+      })
+      break;
+    case 70:
+      musicNotes.forEach(function (oneNote) {
+        if (0 < oneNote.startingY && oneNote.startingY < 100 && 541 < oneNote.startingX && oneNote.startingX < 720) {
+          p = true;
+          // IronGame.ctx.clearRect(this.x, this.y, this.width, this.height);
+        } else if (25 < oneNote.startingY && oneNote.startingY < 50 && 541 < oneNote.startingX && oneNote.startingX < 720) {
+          b = true;
+        }
+      })
+      break;
+  }
+
+  if (p) {
+    newGame.score += 100;
+  } else if (b) {
+    newGame.score += 40;
+  } else {
+    newGame.score -= 25;
+  }
+  updateScore();
+  console.log('your score: ' + newGame.score);
 }
 
 
@@ -148,17 +142,17 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
- 
-btn.onclick = function() {
-    modal.style.display = "block";
+
+btn.onclick = function () {
+  modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  }
 }
